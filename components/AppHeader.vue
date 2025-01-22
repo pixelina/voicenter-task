96b5cfe3e7db6f9bfeac33e3b2d7ed0301866b7c
Polina <polinaromanivna99@gmail.com>
2025-01-22 17:29:21 +0200

    Update AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -63,7 +63,7 @@
         transition-duration: 250ms;
         transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
 
-        &:hover {
+        &.router-link-active, &:hover {
             color: $accent-color;
         }
     }
